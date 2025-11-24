71f02992c2b4a9f1f770e05380bd774b4c472df6
jdra000 <juandavid.rard@gmail.com>
2025-11-24 12:22:19 -0500

    basic repl using lexer

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
-import "fmt"
-
-// List represents a singly-linked list that holds
-// values of any type
-
-type List[T any] struct {
-	next *List[T]
-	val  T
-}
-
-func (l *List[T]) Add(value T) {
-	current := l
-	for current.next != nil {
-		current = current.next
-	}
-	current.next = &List[T]{nil, value}
-}
-
-func (l *List[T]) String() string {
-	return fmt.Sprintf("%v", l.val)
-}
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	list := &List[int]{nil, 1}
-	list.Add(2)
-	list.Add(3)
-
-	for n := list; n != nil; n = n.next {
-		fmt.Println(n)
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
 	}
+	fmt.Printf("Hello %s! This is the Monkey programming language\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
